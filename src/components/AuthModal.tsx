10b0f5eef80a73043414aa6bdc9a07698951d2f5
Rohan O'Dwyer <github@rohanodwyer.com>
2025-06-01 05:38:26 +0200

    Updated App.tsx

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -156,7 +156,7 @@ export const AuthModal: React.FC<AuthModalProps> = ({ isOpen, onClose }) => {
               <div className="text-center text-sm">
                 {authView === 'sign_in' ? (
                   <>
-                    <a href="#forgot" className="text-orange-600 hover:text-orange-700">
+                    <a href="#forgot\" className="text-orange-600 hover:text-orange-700">
                       Forgot your password?
                     </a>
                     <div className="mt-2">
